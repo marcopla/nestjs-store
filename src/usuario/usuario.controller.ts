5ab9ab2537a213ef2c26a532099dfae66752be4c
Marco Pla <marco.pla@gmail.com>
2023-12-12 19:00:06 -0300

    feat(usuario) : module usuario created

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsuarioRepository } from './usuario.repository';
 @Controller('/usuarios')
 export class UsuarioController {
-  private usuarioRepository = new UsuarioRepository();
+  constructor(private usuarioRepository: UsuarioRepository) {}
   @Post()
   async criaUsuario(@Body() dadosDoUsuario) {
     this.usuarioRepository.salvar(dadosDoUsuario);
