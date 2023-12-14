4e69f3a85928af9538539f55de9dbe5b3eec1d99
Marco Pla <marco.pla@gmail.com>
2023-12-14 17:28:25 -0300

    feat(produto) : dto created

---
+++ src/produto/dto/CriaProduto.dto.ts
@@ -0,0 +1,70 @@
+import { Type } from 'class-transformer';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsUrl,
+  MaxLength,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+
+export class CaracteristicasDoProdutoDTO {
+  @IsString()
+  @IsNotEmpty({ message: 'O nome da característica não pode fica vazio.' })
+  nome: string;
+  @IsString()
+  @IsNotEmpty({ message: 'A descrição não pode ficar vazia.' })
+  descricao: string;
+}
+
+export class ImagemDoProdutoDTO {
+  @IsUrl()
+  url: string;
+  @IsString()
+  @IsNotEmpty({ message: 'Descrição deve ser preenchida.' })
+  descricao: string;
+}
+
+export class CriaProdutoDTO {
+  @IsString()
+  @IsNotEmpty({ message: 'Nome do produto não pode ser vazio' })
+  nome: string;
+
+  @IsNumber({ maxDecimalPlaces: 2, allowNaN: false, allowInfinity: false })
+  @Min(1, { message: 'O valor precisa ser maior que zero' })
+  valor: number;
+
+  @IsNumber()
+  @Min(0, { message: 'Quantidade mínima inválida' })
+  quantidadeDisponivel: number;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Descrição do produto não pode ser vazia.' })
+  @MaxLength(1000, { message: 'Descrição do produto não pode ser vazia.' })
+  descricao: string;
+
+  @ValidateNested()
+  @IsArray()
+  @ArrayMinSize(3)
+  @Type(() => CaracteristicasDoProdutoDTO)
+  caracteristicas: CaracteristicasDoProdutoDTO[];
+
+  @ValidateNested()
+  @IsArray()
+  @ArrayMinSize(1)
+  @Type(() => ImagemDoProdutoDTO)
+  imagens: ImagemDoProdutoDTO[];
+
+  @IsString()
+  @IsNotEmpty({ message: 'Categoria do produto não pode ser vazia' })
+  categoria: string;
+
+  @IsDateString()
+  dataCriacao: Date;
+  @IsDateString()
+  dataAtualizacao: Date;
+}
--- src/produto/produto.controller.ts
+++ src/produto/produto.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ProdutoRepository } from './produto.repository';
+import { CriaProdutoDTO } from './dto/CriaProduto.dto';
 
 @Controller('/produtos')
 export class ProdutoController {
   constructor(private produtoRepository: ProdutoRepository) {}
   @Post()
-  async criaProduto(@Body() dadosDoProduto) {
+  async criaProduto(@Body() dadosDoProduto: CriaProdutoDTO) {
     this.produtoRepository.salvar(dadosDoProduto);
     return dadosDoProduto;
   }
