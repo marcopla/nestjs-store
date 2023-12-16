9eb1ecd8d2d98caac5bd820e2a8a05ca06a1a172
Marco Pla <marco.pla@gmail.com>
2023-12-15 22:10:13 -0300

    feat(usuario) : custom unic email validation

--- src/usuario/usuario.repository.ts
+++ src/usuario/usuario.repository.ts
@@ -9,4 +9,10 @@ export class UsuarioRepository {
   async listar() {
     return this.usuarios;
   }
+  async existeComEmail(email: string) {
+    const possivelUsuario = this.usuarios.find(
+      (usuario) => usuario.email === email,
+    );
+    return possivelUsuario !== undefined;
+  }
 }
