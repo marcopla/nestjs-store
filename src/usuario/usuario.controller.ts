8593cb2f810a20e91906976fe2e12e5f51c72518
Marco Pla <marco.pla@gmail.com>
2023-12-17 17:51:41 -0300

    feat(usuario) : ListaUsuario.dto.ts created to @Get show only user id and nome

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -3,6 +3,7 @@ import { UsuarioRepository } from './usuario.repository';
 import { CriaUsuarioDTO } from './dto/CriaUsuario.dto';
 import { UsuarioEntity } from './usuario.entity';
 import { v4 as uuid } from 'uuid';
+import { ListaUsuarioDTO } from './dto/ListaUsuario.dto';
 @Controller('/usuarios')
 export class UsuarioController {
   constructor(private usuarioRepository: UsuarioRepository) {}
@@ -14,10 +15,17 @@ export class UsuarioController {
     usuarioEntity.nome = dadosDoUsuario.nome;
     usuarioEntity.id = uuid();
     this.usuarioRepository.salvar(usuarioEntity);
-    return { id: usuarioEntity.id, message: 'usuário criado com sucesso' };
+    return {
+      usuario: new ListaUsuarioDTO(usuarioEntity.id, usuarioEntity.nome),
+      message: 'usuário criado com sucesso',
+    };
   }
   @Get()
   async listaUsuarios() {
-    return this.usuarioRepository.listar();
+    const usuariosSalvos = await this.usuarioRepository.listar();
+    const usuariosLista = usuariosSalvos.map(
+      (usuario) => new ListaUsuarioDTO(usuario.id, usuario.nome),
+    );
+    return usuariosLista;
   }
 }
