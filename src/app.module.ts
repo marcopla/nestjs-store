28a3bf12b1875b68de61ca42f0b7fdf6269e0c28
Marco Pla <marco.pla@gmail.com>
2023-12-31 17:40:35 -0300

    feat(Postgres) : db config created and added to app.nodule.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
 import { ProdutoModule } from './produto/produto.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './config/postgres.config.services';
 @Module({
-  imports: [UsuarioModule, ProdutoModule],
+  imports: [
+    UsuarioModule,
+    ProdutoModule,
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
 })
 export class AppModule {}
